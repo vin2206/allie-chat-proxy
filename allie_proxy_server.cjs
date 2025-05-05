9c4d8a880e7c31ca095d465dd0134e3520a5995c
vin2206 <factonomy01@gmail.com>
2025-05-06 00:52:11 +0530

    Update allie_proxy_server.cjs

--- allie_proxy_server.cjs
+++ allie_proxy_server.cjs
@@ -1,9 +1,11 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const resendAPIKey = process.env.RESEND_API_KEY;
