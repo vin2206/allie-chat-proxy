6b02d4a1b35733045881970b746db714c7e25bb6
vin2206 <factonomy01@gmail.com>
2025-05-08 01:05:38 +0530

    Update allie_proxy_server.cjs

--- allie_proxy_server.cjs
+++ allie_proxy_server.cjs
@@ -100,7 +100,7 @@ app.get('/', (req, res) => {
   res.send('Allie Chat Proxy is running.');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
