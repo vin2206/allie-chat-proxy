8ac84a5ef4905be3c95a4f90b9f281029b519a58
vin2206 <factonomy01@gmail.com>
2025-05-12 04:46:28 +0530

    Update allie_proxy_server.cjs

--- allie_proxy_server.cjs
+++ allie_proxy_server.cjs
@@ -70,7 +70,7 @@ app.post("/chat", async (req, res) => {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "nothingisreal/mn-celeste-12b",
+        model: "openchat/openchat-3.5-0106",
         messages
       })
     });
